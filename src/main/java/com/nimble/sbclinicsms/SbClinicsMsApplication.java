08ffe87a6739546f20d037def6f2f29e6db21383
maxime.dichiara@intellijam.com <Giggity5>
2020-04-13 12:52:30 +0200

    initial commit

---
+++ src/main/java/com/nimble/sbclinicsms/SbClinicsMsApplication.java
@@ -0,0 +1,13 @@
+package com.nimble.sbclinicsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbClinicsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbClinicsMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nimble/sbclinicsms/SbClinicsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimble.sbclinicsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbClinicsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
